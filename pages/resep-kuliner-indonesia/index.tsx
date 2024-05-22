d400f2d363fba14e4a3433020debb9ba530425fc
JohanesCarlo <repubsepty@yahoo.com>
2024-05-22 08:13:26 +0700

    re-create resep-kuliner-indonesia & remove some unused picture

--- pages/resep-kuliner-indonesia/index.tsx
+++ pages/resep-kuliner-indonesia/index.tsx
@@ -3,44 +3,21 @@ import Navbar from "@/components/Navbar";
 import { footerLeft as Footer } from "@/components/Footer";
 import Image from "next/image";
 import Cards from "@/components/Cards";
-import HeaderCards from "@/components/HeaderCards";
 
 import Thumbnail from "@/assets/images/Resep-Thumbnail.png";
+
 // Sumatera
 import Rendang from "@/assets/images/Resep-Rendang.png";
-import MieAceh from "@/assets/images/Resep-MieAceh.png";
-import Pempek from "@/assets/images/Resep-Pempek.png";
-import Bika from "@/assets/images/Resep-Bika.png";
-
 // Jawa
 import Rawon from "@/assets/images/Resep-Rawon.png";
-import NPecel from "@/assets/images/Resep-NPecel.png";
-import Gudeg from "@/assets/images/Resep-Gudeg.png";
-import Klepon from "@/assets/images/Resep-Klepon.png";
-
 // Kalimantan
 import SotoBanjar from "@/assets/images/Resep-SotoBanjar.png";
-import Patin from "@/assets/images/Resep-Patin.png";
-import KetupatKandangan from "@/assets/images/Resep-KetupatKandangan.png";
-import Bingka from "@/assets/images/Resep-Bingka.png";
-
 // Bali dan Nusa Tenggara
 import Betutu from "@/assets/images/Resep-Betutu.png";
-import Plecing from "@/assets/images/Resep-Plecing.png";
-import Taliwang from "@/assets/images/Resep-Taliwang.png";
-import PieSusu from "@/assets/images/Resep-PieSusu.png";
-
 // Sulawesi
 import Coto from "@/assets/images/Resep-CotoMakassar.png";
-import Woku from "@/assets/images/Resep-Woku.png";
-import Konro from "@/assets/images/Resep-Konro.png";
-import Onde from "@/assets/images/Resep-Onde.png";
-
 // Maluku dan Papua
 import IkanBakarM from "@/assets/images/Resep-IkanManokwari.png";
-import Colo from "@/assets/images/Resep-Colo.png";
-import Papeda from "@/assets/images/Resep-Papeda.png";
-import Ne from "@/assets/images/Resep-Ne.png";
 
 export default function resepKulinerIndonesia() {
   return (
@@ -61,97 +38,85 @@ export default function resepKulinerIndonesia() {
         </div>
       </div>
 
-      <div className="mx-36 my-10">
-        <HeaderCards title="Sumatera" />
-        <div className="flex mx-20">
-          <Cards
-            imageSource={Rendang}
-            altPicture=""
-            name="Rendang"
-            link="/resep-kuliner-indonesia/sumatera/rendang"
-          />
-          <Cards imageSource={MieAceh} altPicture="" name="Mie Aceh" />
-          <Cards imageSource={Pempek} altPicture="" name="Pempek" />
-          <Cards imageSource={Bika} altPicture="" name="Bika Ambon Medan" />
-        </div>
-      </div>
+      <div className="relative">
+        <div className="absolute mt-14 left-0 w-full h-[30px] bg-gradient-to-r from-emerald-900 to-green-950 z-[-1]"></div>
+        <div className="flex flex-col items-center">
+          <div className="my-10">
+            <div className="flex mx-20">
+              <div className="text-center">
+                <h1 className="text-white mb-3 bg-emerald-950 text-3xl p-3 mx-10 rounded-full">
+                  Sumatera
+                </h1>
+                <Cards
+                  imageSource={Rendang}
+                  altPicture=""
+                  name="Rendang"
+                  link="/resep-kuliner-indonesia/sumatera/rendang"
+                />
+              </div>
+              <div className="text-center">
+                <h1 className="text-white mb-3 bg-emerald-950 text-3xl p-3 mx-10 rounded-full">
+                  Jawa
+                </h1>
+                <Cards
+                  imageSource={Rawon}
+                  altPicture=""
+                  name="Rawon"
+                  link="/resep-kuliner-indonesia/jawa/rawon"
+                />
+              </div>
+              <div className="text-center">
+                <h1 className="text-white mb-3 bg-emerald-950 text-3xl p-3 mx-10 rounded-full">
+                  Kalimantan
+                </h1>
+                <Cards
+                  imageSource={SotoBanjar}
+                  altPicture=""
+                  name="Soto Banjar"
+                  link="/resep-kuliner-indonesia/kalimantan/soto-banjar"
+                />
+              </div>
+            </div>
+          </div>
 
-      <div className="mx-36 my-10">
-        <HeaderCards title="Jawa" />
-        <div className="flex mx-20">
-          <Cards
-            imageSource={Rawon}
-            altPicture=""
-            name="Rawon"
-            link="/resep-kuliner-indonesia/jawa/rawon"
-          />
-          <Cards imageSource={NPecel} altPicture="" name="Nasi Pecel" />
-          <Cards imageSource={Gudeg} altPicture="" name="Gudeg Jogja" />
-          <Cards imageSource={Klepon} altPicture="" name="Klepon" />
-        </div>
-      </div>
-
-      <div className="mx-36 my-10">
-        <HeaderCards title="Kalimantan" />
-        <div className="flex mx-20">
-          <Cards
-            imageSource={SotoBanjar}
-            altPicture=""
-            name="Soto Banjar"
-            link="/resep-kuliner-indonesia/kalimantan/soto-banjar"
-          />
-          <Cards imageSource={Patin} altPicture="" name="Patin Bakar Banjar" />
-          <Cards
-            imageSource={KetupatKandangan}
-            altPicture=""
-            name="Ketupat Kandangan"
-          />
-          <Cards imageSource={Bingka} altPicture="" name="Bingka Barandam" />
-        </div>
-      </div>
-
-      <div className="mx-36 my-10">
-        <HeaderCards title="Bali & Nusa Tenggara" />
-        <div className="flex mx-20">
-          <Cards
-            imageSource={Betutu}
-            altPicture=""
-            name="Ayam Betutu"
-            link="/resep-kuliner-indonesia/bali-nt/ayam-betutu"
-          />
-          <Cards imageSource={Plecing} altPicture="" name="Plecing Kangkung" />
-          <Cards imageSource={Taliwang} altPicture="" name="Ayam Taliwang" />
-          <Cards imageSource={PieSusu} altPicture="" name="Pie Susu Bali" />
-        </div>
-      </div>
-
-      <div className="mx-36 my-10">
-        <HeaderCards title="Sulawesi" />
-        <div className="flex mx-20">
-          <Cards
-            imageSource={Coto}
-            altPicture=""
-            name="Coto Makassar"
-            link="/resep-kuliner-indonesia/sulawesi/coto-makassar"
-          />
-          <Cards imageSource={Woku} altPicture="" name="Ikan Woku Belanga" />
-          <Cards imageSource={Konro} altPicture="" name="Sop Konro" />
-          <Cards imageSource={Onde} altPicture="" name="Onde - Onde" />
-        </div>
-      </div>
-
-      <div className="mx-36 my-10">
-        <HeaderCards title="Maluku dan Papua" />
-        <div className="flex mx-20">
-          <Cards
-            imageSource={IkanBakarM}
-            altPicture=""
-            name="Ikan Bakar Manokwari"
-            link="/resep-kuliner-indonesia/maluku-papua/ikan-bakar-manokwari"
-          />
-          <Cards imageSource={Colo} altPicture="" name="Sambal Colo-Colo" />
-          <Cards imageSource={Papeda} altPicture="" name="Papeda" />
-          <Cards imageSource={Ne} altPicture="" name="Bubur Ne Maluku" />
+          <div className="my-10">
+            <div className="absolute mt-4 left-0 w-full h-[30px] bg-gradient-to-r from-emerald-900 to-green-950 z-[-1]"></div>
+            <div className="flex mx-20">
+              <div className="text-center">
+                <h1 className="text-white mb-3 bg-emerald-950 text-3xl p-3 mx-10 rounded-full">
+                  Bali & Nusa Tenggara
+                </h1>
+                <Cards
+                  imageSource={Betutu}
+                  altPicture=""
+                  name="Ayam Betutu"
+                  link="/resep-kuliner-indonesia/bali-nt/ayam-betutu"
+                />
+              </div>
+              <div className="text-center">
+                <h1 className="text-white mb-3 bg-emerald-950 text-3xl p-3 mx-10 rounded-full">
+                  Sulawesi
+                </h1>
+                <Cards
+                  imageSource={Coto}
+                  altPicture=""
+                  name="Coto Makassar"
+                  link="/resep-kuliner-indonesia/sulawesi/coto-makassar"
+                />
+              </div>
+              <div className="text-center">
+                <h1 className="text-white mb-3 bg-emerald-950 text-3xl p-3 mx-10 rounded-full">
+                  Maluku & Papua
+                </h1>
+                <Cards
+                  imageSource={IkanBakarM}
+                  altPicture=""
+                  name="Ikan Bakar Manokwari"
+                  link="/resep-kuliner-indonesia/maluku-papua/ikan-bakar-manokwari"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
